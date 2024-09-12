c19f809a2894d189e68dfb7b8bc78db84b899f84
keito suwahara <keitosuwahara0816@gmail.com>
2024-09-12 20:45:09 +0900

    temp

--- object/environment.go
+++ object/environment.go
@@ -1,17 +1,29 @@
 package object
 
 
+type Environment struct {
+	store map[string]Object
+	outer *Environment
+}
+
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{store: s}
 }
 
-type Environment struct {
-	store map[string]Object
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
+
 
 func (env *Environment) Get(name string) (Object, bool) {
 	obj, ok := env.store[name]
+	if !ok && env.outer != nil {
+		obj, ok = env.outer.Get(name)
+	}
 	return obj, ok
 }
 
