06b4b1d05623ee9bb885fd62815aa42e2033788d
keitosuwahara <210103gi@yse-c.net>
2024-09-13 06:56:19 +0900

    func env added

--- eval/eval.go
+++ eval/eval.go
@@ -84,11 +84,43 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return args[0]
 		}
 
-		return applyFunction
+		return applyFunction(function, args)
 	}
 
 	return nil
 }
+
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+	return unwrapReturnValue(evaluated)
+}
+
+
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+
+	return obj
+}
+
 
 func evalExpression(exps []ast.Expression, env *object.Environment) []object.Object {
 	var result []object.Object
